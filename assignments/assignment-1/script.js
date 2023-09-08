e65f26c8b530c696a6654c8e00d751563e730ef1
Rukshona Rad <rukshona.rad@gmail.com>
2023-09-08 19:09:34 -0400

    Finish assignment-1

--- assignments/assignment-1/script.js
+++ assignments/assignment-1/script.js
@@ -1,43 +1,97 @@
-const { log } = require("console");
 const fs = require("fs");
-const { domainToASCII } = require("url");
 class FinancialAnalytics {
-    readFile() {
-        const content = fs.readFileSync("data.json", "utf-8");
-        return JSON.parse(content);
-    }
-    writeFile(data) {
-        fs.writeFileSync("data.json", JSON.stringify(data));
-    }
-    readJSONData(filename, dataCallback, writeCallback) {
-        fs.readFile(filename, "utf8", (err, rawData) => {
+    static writeFile(fileUrl, data) {
+        fs.writeFile(fileUrl, JSON.stringify(data), (err) => {
             if (err) {
-                throw Error("Somthink wrong");
+                throw err;
             }
-            const jsonData = JSON.parse(rawData);
-            const transformedData = dataCallback(jsonData);
-            writeCallback(transformedData);
-
-            const updateData = FinancialAnalytics(transformedData);
         });
     }
-
-    calculateMonthlySavings(content) {
-        const dataObj = content;
-        let total = 0;
-        for (const month in dataObj) {
-            const expensisCategory = dataObj[month];
-            for (const category in expensisCategory) {
-                total += expensisCategory[category];
+    static readJSONData(filename, dataCallback, writeCallback) {
+        fs.readFile(filename, "utf8", (err, data) => {
+            if (err) throw err;
+            const parseData = JSON.parse(data);
+            const transformedData = dataCallback(parseData);
+            writeCallback("newData.json", transformedData);
+        });
+    }
+    init(fileUrl, transformedDataFunc) {
+        FinancialAnalytics.readJSONData(
+            fileUrl,
+            transformedDataFunc,
+            FinancialAnalytics.writeFile
+        );
+    }
+    static calculateMonthlySavings(data) {
+        let monthlySavings = {};
+        for (const month in data) {
+            monthlySavings[month] = {};
+            for (const category in data[month].incomes) {
+                const income = data[month].incomes[category];
+                const expenses = data[month].expenses[category];
+                const totally =
+                    income -
+                    (expenses.food +
+                        expenses.rent +
+                        expenses.entertainment +
+                        expenses.transportation);
+                monthlySavings[month][category] = totally;
             }
         }
-        const savings = total;
-        return { expenses: total, savings };
-        fs.writeFile("data.json".JSON.stringify(dataObj));
+        return monthlySavings;
+    }
+    static compareMonthlyExpenses(data) {
+        let monthlyExp = {};
+        for (let monthly in data[1].expenses) {
+            monthlyExp[monthly] = {};
+            // const items = [food, rent, entertainment, transportation];
+            for (let season in data[1].expenses[monthly]) {
+                let maxExp = {
+                    id: 1,
+                    amount: data[1].expenses[monthly][season]
+                };
+                let minExp = {
+                    id: 1,
+                    amount: data[1].expenses[monthly][season]
+                };
+                for (let num in data) {
+                    var expense = data[num].expenses[monthly][season];
+                }
+                if (expense > maxExp.amount) {
+                    maxExp.id = monthly.id;
+                    maxExp.amount = expense;
+                }
+                if (expense < minExp.amount) {
+                    minExp.id = monthly.id;
+                    minExp.amount = expense;
+                }
+                monthlyExp[monthly][season] = {
+                    max: maxExp,
+                    min: minExp
+                };
+            }
+        }
+        return monthlyExp;
+    }
+    static calculateAnnualExpenses(data) {
+        let annualExp = {};
+        for (let personId in data) {
+            annualExp[personId] = {};
+            for (const category in data[personId].expenses[
+                Object.keys(data[personId].expenses)[0]
+            ]) {
+                let isTotal = 0;
+                for (const month in data[personId].expenses) {
+                    isTotal = +data[personId].expenses[month][category];
+                }
+                annualExp[personId][category] = isTotal;
+            }
+        }
+        return annualExp;
     }
 }
-
-const data = new FinancialAnalytics();
-console.log(data.readFile());
-// console.log(data.readJSONData());
-console.log(data.calculateMonthlySavings("data.json", data.readFile));
+const analytics = new FinancialAnalytics();
+analytics.init("data.json", FinancialAnalytics.compareMonthlyExpenses);
+console.log(FinancialAnalytics.readJSONData());
+console.log(FinancialAnalytics.calculateMonthlySavings());
+// console.log(analytics.readJSONData());
