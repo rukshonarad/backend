797bb79011bef4e5815e710b9334d46f37e284bd
Rukshona Rad <rukshona.rad@gmail.com>
2023-09-14 13:36:59 -0400

    Partialy done assignment-4

---
+++ assignments/assignment-4/data.js
@@ -0,0 +1,48 @@
+export const stories = {
+    "2c81aa6b-4edd-41d4-84db-9920fbb5a8b2": {
+        id: "2c81aa6b-4edd-41d4-84db-9920fbb5a8b2",
+        name: "To Kill a Mockingbird",
+        genre: "Classic Fiction",
+        author: "Harper Lee",
+        tasks: [
+            {
+                id: "8f7ebf20-dfb8-4a53-8430-14f62ee0f37e",
+                text: "Some text",
+                rating: 4
+            },
+            {
+                id: "022ba40c-272e-4bc0-922d-ec8a2bb56379",
+                text: "Some text1",
+                rating: 5
+            },
+            {
+                id: "5c5e559a-db88-4780-8e38-be19ab0e3f32",
+                text: "Some text2",
+                rating: 5
+            }
+        ]
+    },
+    "f2fc9e73-0bbf-460b-9638-9db21d4a9e5f": {
+        id: "f2fc9e73-0bbf-460b-9638-9db21d4a9e5f",
+        name: "1984",
+        genre: "Classic Fiction",
+        author: "George Orwell",
+        tasks: [
+            {
+                id: "df42bd67-ed38-421c-ab1a-ff16cf54d808",
+                text: "Some text",
+                rating: 4
+            },
+            {
+                id: "58fd18c8-d78d-4ebc-8758-d6536421b52d",
+                text: "Some text1",
+                rating: 5
+            },
+            {
+                id: "043ed035-429d-48a8-8ed9-1e8a1b6ebe66",
+                text: "Some text2",
+                rating: 5
+            }
+        ]
+    }
+};
---
+++ assignments/assignment-4/server.js
@@ -0,0 +1,78 @@
+import express from "express";
+import { stories } from "./data.js";
+import { validate, v4 as uuid } from "uuid";
+const app = express();
+app.use(express.json());
+
+const PORT = 5020;
+
+//Get all stories
+app.get("/stories", (req, res) => {
+    res.status(200).json({
+        data: stories
+    });
+});
+//Get stories by id
+app.get("/stories/:storyId", (req, res) => {
+    const storyId = req.params.storyId;
+
+    res.status(200).json({
+        massage: stories[storyId]
+    });
+});
+//Delete story by id
+app.delete("/stories/:storyId", (req, res) => {
+    const storyId = req.params.storyId;
+    if (!validate(storyId) || !stories[storyId]) {
+        return res.status(400).json({ message: "Not a valid story ID" });
+    }
+    delete stories[storyId];
+    res.status(204).send();
+});
+//Update Story
+app.put("/stories/:storyId", (req, res) => {
+    const storyId = req.params.storyId;
+    const updatedData = req.body;
+    if (!validate(storyId) || !stories[storyId]) {
+        return res.status(400).json({ message: "Not a valid story ID" });
+    }
+    stories[storyId] = { ...stories[storyId], ...updatedData };
+    res.status(200).json({ data: stories[storyId] });
+});
+
+//Get all subtasks
+app.get("./stories/:storyId/tasks"),
+    (req, res) => {
+        const storyId = req.params.storyId;
+        if (!validate(storyId) || !stories[storyId]) {
+            return res.status(400).json({ message: "Not a valid story ID" });
+        }
+        req.status(200).json({
+            data: stories[storyId].tasks
+        });
+    };
+
+//Create new subtask
+app.post("./stories/:storyId/tasks"),
+    (req, res) => {
+        const storyId = req.params.storyId;
+        const reviewData = req.body;
+        const id = uuid();
+        if (!validate(storyId) || !stories[storyId]) {
+            return res.status(400).json({ message: "Not a valid story ID" });
+        }
+        const newTask = {
+            id,
+            ...reviewData
+        };
+        stories[storyId].tasks.push(reviewData);
+        req.status(201).json({
+            data: newTask
+        });
+    };
+//////---------------
+
+//-------------------------//
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
