30524c2e6965d6f844c91f7987f2144347cfa8ee
Rukshona Rad <rukshona.rad@gmail.com>
2023-11-02 20:05:11 -0400

    Solve array and srting problems

--- lectures/interview/question-5/index.js
+++ lectures/interview/question-5/index.js
@@ -26,3 +26,29 @@ console.log(isValidBrace("{}[()]"));
 console.log(isValidBrace("[}"));
 console.log(isValidBrace("[})"));
 console.log(isValidBrace("(){}[]"));
+
+// Construct a function that counts the number of vowels in a string.
+
+const countVowels = (str) => {
+    let count = 0;
+    let vowels = "aeiou";
+    for (const char of str) {
+        if (vowels.includes(char)) count++;
+    }
+    return { vovels: count, consonantnts: str.length - count };
+};
+
+console.log(countVowels("apple"));
+
+// [1,2,3,4,5,6,7,8,9,10]
+// Sum up array. but do not use loop
+// 1, 2, 3 = 6; 3 * 4 / 2 = 6
+// 1, 2, 3, 4 = 10; 4 * 5 / 2 = 10
+// 1, 2, 3, 4, 5 = 15; 5 * 6  / 2 = 15;
+
+// (n * (n +1) )/2
+
+const sumUpArr = (arr) => {
+    return (arr.length * (arr.length + 1)) / 2;
+};
+console.log(sumUpArr([1, 2, 3, 4]));
